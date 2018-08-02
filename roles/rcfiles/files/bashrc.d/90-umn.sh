e2c0bb0c0202642de8260221522dfde8163f53d7
John Trammell <tram0004@umn.edu>
2018-08-02 17:13:42 -0500

    added lastpass call to set up UMN development env

---
+++ roles/rcfiles/files/bashrc.d/90-umn.sh
@@ -0,0 +1,7 @@
+# configure UMN development environment
+
+# set a safe path
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:${HOME}/.local/bin
+
+# set lastpass environment variables
+eval "$(lpass show --notes 193464738201392340193728201)"
