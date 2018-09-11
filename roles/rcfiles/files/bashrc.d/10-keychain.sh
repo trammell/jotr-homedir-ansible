016acb0959302b20a1c93d89eeda2c61bf197436
John Trammell <tram0004@umn.edu>
2018-09-10 20:45:21 -0500

    relocated ordered load files

--- roles/rcfiles/files/bashrc.d/10-keychain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Re-use ssh-agent and/or gpg-agent between logins:
-#   * http://www.funtoo.org/Keychain
-#   * http://www.cyberciti.biz/faq/ssh-passwordless-login-with-keychain-for-scripts/
-
-eval $(keychain --agents gpg,ssh --eval --quiet tram0004 tram0004-oit)
